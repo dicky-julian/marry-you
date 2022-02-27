685bfb3988740d47a62342a0188dd31bb443b6bb
Rhesa Daiva Bremana <rhesa.daiva@gmail.com>
2022-02-27 19:42:07 +0700

    add new concept of audio

--- src/layouts/components/navbar/navbar.js
+++ src/layouts/components/navbar/navbar.js
@@ -1,46 +1,38 @@
 import React, { useEffect, useState } from "react";
-// import audioFile from '/audio/audio_2.mp3'
-import audioFile from '../../../assets/audio/audio_2.mp3';
 
+const useAudio = (url) => {
 
-// const useAudio = () => {
+  const [audio] = useState(new Audio(url));
+  const [playing, setPlaying] = useState(true);
 
-//   const [audio] = useState(new Audio(url));
-//   const [playing, setPlaying] = useState(false);
+  // audio.autoplay = true
 
-//   const toggle = () => setPlaying(!playing);
+  const toggle = () => setPlaying(!playing);
 
-//   useEffect(() => {
-//     playing ? audio.play() : audio.pause() 
+  useEffect(() => {
+    playing ? audio.play() : audio.pause() 
+  }, [playing]);
 
-//     if (audio.current?.paused && audio.current?.currentTime > 0 && audio.current?.ended) {
-//       audio.current?.play();
-//     } else if (audio.current?.ended) {
-//       audio.current?.play();
-//     } else {
-//       audio.current?.pause();
-//     }
-//   }, [playing, audio]);
+  useEffect(() => {
+    audio.addEventListener("ended", () => setPlaying(true));
+    window.addEventListener("touchstart", () => {
+      audio.play()
+    }, {passive: true})
+    return () => {
+      audio.removeEventListener("ended", () => setPlaying(false));
+      audio.removeEventListener("touchstart",(() =>{
+        audio.pause();
+        console.log("touchstart event removed!")
+      }))
+    };
+  }, []);
 
-//   useEffect(() => {
-//     audio.addEventListener("ended", () => setPlaying(true));
-//     return () => {
-//       audio.removeEventListener("ended", () => setPlaying(false));
-//     };
-//   }, [audio]);
-
-//   return [playing, toggle];
-// };
+  return [playing, toggle];
+};
 
 export const Navbar = () => {
   const [indexScroll, setIndexScroll] = useState(1);
-  const [playAudio, setPlayAudio] = useState(true);
-
-  const [audio] = useState(new Audio(audioFile));
-
-  audio.autoplay = true;
-
-  const toggle = () => setPlayAudio(!playAudio);
+  const [playing, toggle] = useAudio("/audio/audio_2.mp3");
 
   const handleScroll = () => {
     setIndexScroll(window.pageYOffset);
@@ -50,25 +42,6 @@ export const Navbar = () => {
     window.addEventListener("scroll", handleScroll);
     return () => window.removeEventListener("scroll", handleScroll);
   });
-
-  useEffect(() => {
-     playAudio ? audio.play() : audio.pause();
-     
-     if (audio.current?.paused && audio.current?.currentTime > 0  && audio.current?.ended) {
-        audio.current?.play();
-     } else if (audio.current?.ended) {
-       audio.current?.play();
-     } else {
-       audio.current?.pause();
-     }
-  }, [playAudio, audio]);
-
-  useEffect(() => {
-    audio.addEventListener("ended", () => setPlayAudio(true));
-    return () => {
-      audio.removeEventListener("ended", () => setPlayAudio(false));
-    };
-  }, [audio]);
 
   return (
     <>
@@ -94,7 +67,7 @@ export const Navbar = () => {
         className={`toggle-audio ${indexScroll > 64 ? "active" : ""}`}
         onClick={toggle}
       >
-        {playAudio ? (
+        {playing ? (
           <img src="img/icons/icon-stop.webp" />
         ) : (
           <img src="img/icons/icon-play.webp" />
