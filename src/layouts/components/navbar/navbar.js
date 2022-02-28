fd2c0da5f5d34b44e5904abcbc36f22c364f3795
Rhesa Daiva Bremana <rhesa.daiva@gmail.com>
2022-02-28 14:25:11 +0700

    tes new concept audio

--- src/layouts/components/navbar/navbar.js
+++ src/layouts/components/navbar/navbar.js
@@ -1,38 +1,41 @@
 import React, { useEffect, useState } from "react";
 import { BrowserDetection } from "../../../config/browser-detection";
+import audioFile from "../../../assets/audio/audio_2.mp3";
 
-const useAudio = (url) => {
 
-  const [audio] = useState(new Audio(url));
-  const [playing, setPlaying] = useState(false);
-
-  const toggle = () => setPlaying(!playing);
-
-  useEffect(() => {
-    console.log("Playing status: ", playing);
-    playing ? audio.play() : audio.pause() 
-  }, [playing, audio]);
-
-  useEffect(() => {
-    // Check browser compatibility, if safari, sound must be played manually;
-    if(BrowserDetection(window.navigator.userAgent) === "Apple Safari") {
-      setPlaying(false);
-    } else {
-      setPlaying(true);
-    };
-
-    audio.addEventListener("ended", () => setPlaying(true));
-    return () => {
-      audio.removeEventListener("ended", () => setPlaying(false));
-    };
-  }, [audio]);
-
-  return [playing, toggle];
-};
 
 export const Navbar = () => {
   const [indexScroll, setIndexScroll] = useState(1);
-  const [playing, toggle] = useAudio("/audio/audio_2.mp3");
+  // const [playing, toggle] = useAudio("/audio/audio_2.mp3");
+
+  const [playAudio, setPlayAudio] = useState(false);
+  const [audio] = new Audio(audioFile);
+
+  useEffect(() => {
+    playAudio ? audio.play() : audio.pause();
+
+    if (audio.current?.paused && audio.current?.currentTime > 0 && audio.current?.ended) {
+      audio.current?.play();
+    } else if (audio.current?.ended) {
+      audio.current?.play();
+    } else {
+      audio.current?.pause();
+    }
+  }, [playAudio, audio]);
+
+  useEffect(() => {
+    audio.addEventListener("ended", () => setPlayAudio(true));
+    return () => {
+      audio.removeEventListener("ended", () => setPlayAudio(false));
+    };
+  }, [audio]);
+
+  const toggle = (e) => {
+    e.preventDefault();
+    setPlayAudio(!playAudio);
+  }
+
+
   const handleScroll = () => {
     setIndexScroll(window.pageYOffset);
   };
@@ -64,9 +67,10 @@ export const Navbar = () => {
       </nav>
       <div
         className={`toggle-audio ${indexScroll > 64 ? "active" : ""}`}
-        onClickCapture={toggle}
+        onClick={toggle}
+
       >
-        {playing ? (
+        {playAudio ? (
           <img src="img/icons/icon-stop.webp" alt="icon-stop" />
         ) : (
           <img src="img/icons/icon-play.webp" alt="icon-play" />
